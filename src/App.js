cd64c7f8a3a142b6a472c525455557123c96b05d
dovimaj <dovimajowka@gmail.com>
2020-12-20 23:51:37 +0200

    images and styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import Form from "./components/Form.js";
+import Note from "./components/Note";
 
 function App() {
+  const [notes, setNotes] = useState([
+    { title: "dvs", note: "hi", project: "slsl", date: "sdvsdv" },
+    { title: "dvs", note: "hi", project: "slsl", date: "sdvsdv" },
+  ]);
+  const handleForm = (note) => {
+    console.log(note);
+    setNotes([...notes, { note }]);
+  };
   return (
     <div className="App">
-      <Form />
+      <Form handleForm={(note) => handleForm(note)} />
+      <div className="notes-container">
+        {notes.map((note, index) => (
+          <Note key={index} note={note} />
+        ))}
+      </div>
     </div>
   );
 }
