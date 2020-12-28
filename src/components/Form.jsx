49cc8120e16496139c913692741abf610e7ffdc0
dovimaj <dovimajowka@gmail.com>
2020-12-28 21:45:49 +0200

    style improvments: responsive header

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -41,68 +41,70 @@ function Form(props) {
   };
 
   return (
-    <div className="form">
-      {display ? (
-        <input
-          className="input-field title-input"
-          placeholder="Title"
-          name="title"
-          value={note.title}
-          aria-label="title-input"
+    <div className="form-wrapper">
+      <div className="form">
+        {display ? (
+          <input
+            className="input-field title-input"
+            placeholder="Title"
+            name="title"
+            value={note.title}
+            aria-label="title-input"
+            onChange={(e) => handleChange(e)}
+          ></input>
+        ) : null}
+
+        <textarea
+          className="input-field"
+          id="note-input"
+          placeholder="Take a note..."
+          name="note"
+          value={note.note}
+          aria-label="note-input"
+          onClick={() => setDisplay(true)}
           onChange={(e) => handleChange(e)}
-        ></input>
-      ) : null}
+        ></textarea>
 
-      <textarea
-        className="input-field"
-        id="note-input"
-        placeholder="Take a note..."
-        name="note"
-        value={note.note}
-        aria-label="note-input"
-        onClick={() => setDisplay(true)}
-        onChange={(e) => handleChange(e)}
-      ></textarea>
+        {display ? (
+          <ListInput handleListInput={(value) => handleListInput(value)} />
+        ) : null}
 
-      {display ? (
-        <ListInput handleListInput={(value) => handleListInput(value)} />
-      ) : null}
+        {note.list.map((item) => (
+          <li className="temporary-form-list-item" key={uniqid()}>
+            {item.name}
+          </li>
+        ))}
 
-      {note.list.map((item) => (
-        <li className="temporary-form-list-item" key={uniqid()}>
-          {item.name}
-        </li>
-      ))}
-
-      {display ? (
-        <div id="form-bottom-input">
-          <input
-            type="date"
-            className="input-field"
-            id="due-date-input"
-            aria-label="date-input"
-            value={note.date}
-            name="date"
-            onChange={(e) => handleChange(e)}
-          ></input>{" "}
-          <input
-            className="input-field"
-            id="project-input"
-            placeholder="project"
-            aria-label="project-input"
-            value={note.project}
-            name="project"
-            onChange={(e) => handleChange(e)}
-          ></input>{" "}
-          <button
-            aria-label="add-button"
-            id="add-button"
-            onClick={handleSubmit}
-          >
-            Close
-          </button>
-        </div>
-      ) : null}
+        {display ? (
+          <div id="form-bottom-input">
+            <input
+              type="date"
+              className="input-field"
+              id="due-date-input"
+              aria-label="date-input"
+              value={note.date}
+              name="date"
+              onChange={(e) => handleChange(e)}
+            ></input>{" "}
+            <input
+              className="input-field"
+              id="project-input"
+              placeholder="project"
+              aria-label="project-input"
+              value={note.project}
+              name="project"
+              onChange={(e) => handleChange(e)}
+            ></input>{" "}
+            <button
+              aria-label="add-button"
+              id="add-button"
+              onClick={handleSubmit}
+            >
+              Close
+            </button>
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 }
